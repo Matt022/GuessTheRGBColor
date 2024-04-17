800decd63449bc69426411e390f81f0537508d5d
Matus Benus <matusbenus02@gmail.com>
2024-04-17 22:38:29 +0200

    Added randomize function to change background color in GuessComponent

--- src/app/components/guess/guess.component.ts
+++ src/app/components/guess/guess.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-guess',
-  templateUrl: './guess.component.html',
-  styleUrls: ['./guess.component.scss']
+    selector: 'app-guess',
+    templateUrl: './guess.component.html',
+    styleUrls: ['./guess.component.scss']
 })
 export class GuessComponent {
-
+    backgroundColor: string = 'rgb(0, 0, 0)';
+    
+    randomize(): void {
+        const red: number = Math.floor(Math.random() * 256);
+        const green: number = Math.floor(Math.random() * 256);
+        const blue: number = Math.floor(Math.random() * 256);
+        this.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+        console.log(this.backgroundColor);
+    };
 }
