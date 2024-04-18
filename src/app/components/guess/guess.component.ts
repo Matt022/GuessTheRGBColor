0b10e01cfb84fc36ec585de91f75483eff142060
Matus Benus <matusbenus02@gmail.com>
2024-04-18 23:06:01 +0200

    Added countdown timer for guessing, updated styling and score tracking in GuessComponent

--- src/app/components/guess/guess.component.ts
+++ src/app/components/guess/guess.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
     selector: 'app-guess',
@@ -21,6 +23,20 @@ export class GuessComponent {
     private _green: number = 0;
     private _blue: number = 0;
 
+    yourScore: number = 0;
+    count: number = 3;
+    show: boolean = false;
+
+    private subscription: Subscription | undefined;
+
+    ngOnInit(): void {
+        // this.subscription = interval(1000).subscribe(() => {
+        //     if (this.count > 0) {
+        //         this.count--;
+        //     }
+        // });
+    }
+
     randomize(): void {
         this._red = Math.floor(Math.random() * 256);
         this._green = Math.floor(Math.random() * 256);
@@ -29,11 +45,27 @@ export class GuessComponent {
     };
 
     guess(): void {
-        this.userGuessColor = `rgb(${this.userRed}, ${this.userGreen}, ${this.userBlue})`;
         setTimeout(() => {
             this.guessRed = this._red;
             this.guessGreen = this._green;
             this.guessBlue = this._blue;
+            this.userGuessColor = `rgb(${this.userRed}, ${this.userGreen}, ${this.userBlue})`;
+
+            this.show = true;
+
+            this.subscription = interval(1000).subscribe(() => {
+                if (this.count > 0) {
+                    this.count--;
+                }
+            });
         }, 3000);
+
+        this.yourScore = 100;
+    }
+
+    ngOnDestroy(): void {
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+        }
     }
 }
